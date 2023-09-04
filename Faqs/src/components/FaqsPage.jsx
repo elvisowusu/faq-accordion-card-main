36571cf5eafd73ad5ceea3ec9070710e6082b303
elvisowusu <owusuelvisgyasi@gmail.com>
2023-09-04 22:11:05 +0000

    added two divs was shot

--- Faqs/src/components/FaqsPage.jsx
+++ Faqs/src/components/FaqsPage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import '../styles/FaqsPage.css'
+import image from "../assets/"
 
 export const FaqsPage =()=>{
     return(
         <div className="mainPage">
-            
+            <div className="picture">
+
+            </div>
+            <div className="faqs">
+
+            </div>
         </div>
     )
 }
