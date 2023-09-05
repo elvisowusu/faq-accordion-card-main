4954edd308922906c06382e026aff46f906b1b93
elvisowusu <owusuelvisgyasi@gmail.com>
2023-09-05 23:15:27 +0000

    imported images

--- Faqs/src/components/FaqsPage.jsx
+++ Faqs/src/components/FaqsPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import '../styles/FaqsPage.css'
-import image from "../assets/"
+import image from "../assets/bg-pattern-destop.svg"
+import image2 from "../assets/bg-pattern-mobile.svg"
 
 export const FaqsPage =()=>{
     return(
