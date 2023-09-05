ef763674099622b24f5b138b3eced290ff3cda6e
elvisowusu <owusuelvisgyasi@gmail.com>
2023-09-05 23:33:43 +0000

    imported all the images

--- Faqs/src/components/FaqsPage.jsx
+++ Faqs/src/components/FaqsPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import '../styles/FaqsPage.css'
-import image from "../assets/bg-pattern-destop.svg"
-import image2 from "../assets/bg-pattern-mobile.svg"
+import desktopImage from "../assets/bg-pattern-desktop.svg"
+import mobileImage from "../assets/bg-pattern-mobile.svg"
+import box from "../assets/illustration-box-desktop.svg"
+import desktopWoman from "../assets/illustration-woman-online-desktop.svg"
+import mobileWoman from "../assets/illustration-woman-online-mobile.svg"
+
+
 
 export const FaqsPage =()=>{
     return(
