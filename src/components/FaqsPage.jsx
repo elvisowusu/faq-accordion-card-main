266031d5893daad50ae3914b882307256ee3d338
elvisowusu <owusuelvisgyasi@gmail.com>
2023-09-07 07:33:59 +0000

    rearranged the faqs messages

--- src/components/FaqsPage.jsx
+++ src/components/FaqsPage.jsx
@@ -21,21 +21,30 @@ export const FaqsPage =()=>{
                 <img className="box" src={box} alt="" />
             <div className="faqs">
                 <h1>FAQ</h1>
+                    <div className="faqsdiv">
+                        <p className="question">How many team members can I invite?</p>
+                             <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    </div>
 
-                <p className="question">How many team members can I invite?</p>
-                  <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    <div className="faqsdiv">
+                        <p className="question">What is the maximum file upload size?</p>
+                             <p className="sub">No more than 2GB. All files in your account must fit your allotted storage space</p>
+                     </div>
 
-               <p className="question">What is the maximum file upload size?</p>
-                  <p className="sub">No more than 2GB. All files in your account must fit your allotted storage space</p>
+                     <div className="faqsdiv">
+                        <p className="question">How do I reset my password?</p>
+                             <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                     </div>
+
+                     <div className="faqsdiv">
+                        <p className="question">Can I cancel my subscription?</p>
+                             <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                     </div>
                
-               <p className="question">How do I reset my password?</p>
-                  <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-               
-               <p className="question">Can I cancel my subscription?</p>
-                  <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-               
-               <p className="question">Do you provide additional support?</p>
-                  <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                     <div className="faqsdiv">
+                        <p className="question">Do you provide additional support?</p>
+                             <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                     </div>
             </div>
         </div>
         </div>
