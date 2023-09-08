9153dc23bbed280881b494e30523cfe7f523a8fa
elvisowusu <owusuelvisgyasi@gmail.com>
2023-09-08 10:54:43 +0000

    added an open useState

--- src/components/FaqsPage.jsx
+++ src/components/FaqsPage.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/FaqsPage.css'
 import desktopImage from "../assets/bg-pattern-desktop.svg"
 import mobileImage from "../assets/bg-pattern-mobile.svg"
 import box from "../assets/illustration-box-desktop.svg"
 import desktopWoman from "../assets/illustration-woman-online-desktop.svg"
 import mobileWoman from "../assets/illustration-woman-online-mobile.svg"
-import {IoIosArrowDropdown} from "react-icons/io"
+import {RiArrowDownSLine} from "react-icons/ri"
 
 
 export const FaqsPage =()=>{
+    const [open, setOpen] = useState(false);
+    const [show, setShow] = useState(true);
+
+    const handleArrowClick=()=>{
+        setOpen(true);
+    }
+
+    const styles ={
+        transform:`rotate(${180}deg)`,
+        transition: `transform ${0.5}s ease`,
+    }
+
+
+
+
     return(
         <div className="relative">
         
@@ -22,34 +37,59 @@ export const FaqsPage =()=>{
             <div className="faqs">
                 <h1>FAQ</h1>
                     <div className="faqsdiv">
-                    <div><p className="question">How many team members can I invite?</p><button><IoIosArrowDropdown/></button> </div>
-                             <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                     <div>
+                        <p className="question">How many team members can I invite?</p>
+                        <button onClick={handleArrowClick} >
+                            < RiArrowDownSLine className="arrow"/>
+                        </button> 
+                     </div>
+                            {open === show? <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>:""} 
                         <hr />
                     </div>
 
                     <div className="faqsdiv">
-                    <div><p className="question">What is the maximum file upload size?</p><button><IoIosArrowDropdown/></button>  </div>
+                     <div>
+                        <p className="question">What is the maximum file upload size?</p>
+                        <button>
+                            <RiArrowDownSLine className="arrow"/>
+                        </button> 
+                     </div>
                              <p className="sub">No more than 2GB. All files in your account must fit your allotted storage space</p>
                         <hr />
-                     </div>
+                    </div>
 
                      <div className="faqsdiv">
-                        <div><p className="question">How do I reset my password?</p><button><IoIosArrowDropdown/></button>  </div>
+                        <div>
+                            <p className="question">How do I reset my password?</p>
+                            <button>
+                                <RiArrowDownSLine className="arrow"/>
+                            </button> 
+                        </div>
                              <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         <hr />
                      </div>
 
                      <div className="faqsdiv">
-                     <div><p className="question">Can I cancel my subscription?</p><button><IoIosArrowDropdown/></button>  </div>
+                      <div>
+                        <p className="question">Can I cancel my subscription?</p>
+                        <button>
+                            <RiArrowDownSLine className="arrow"/>
+                        </button>  
+                      </div>
                              <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         <hr />
                      </div>
 
                      <div className="faqsdiv">
-                     <div><p className="question">Do you provide additional support?</p><button><IoIosArrowDropdown/></button>  </div>
+                      <div>
+                        <p className="question">Do you provide additional support?</p>
+                        <button>
+                            <RiArrowDownSLine className="arrow"/>
+                        </button>  
+                      </div>
                              <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         <hr />
-                     </div>
+                    </div>
             </div>
         </div>
         </div>
